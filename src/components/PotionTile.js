f6a5ac9ac98df347b2314e0278e4dc0dc475eb5d
Silvanos Eric <ericoderito@gmail.com>
2024-08-01 10:17:20 +0300

    refactor(components): unify InventoryTile and ShopTile into a single PotionTile component
    - Replace InventoryTile with PotionTile in Inventory component
      - Update to pass handleClick prop instead of sell
    - Replace ShopTile with PotionTile in Shop component
      - Update to pass handleClick prop instead of addToInventory
    - Implement PotionTile component
      - Add conditional class based on handleClick function name
      - Handle click event to invoke handleClick with potion id

--- src/components/PotionTile.js
+++ src/components/PotionTile.js
@@ -8,8 +8,19 @@ Consider what the tiles have in common and what they do not:
 - For things that are different, consider how you might make use of props and JSX to have the same component behave differently when rendered by different parents 
 */
 
-function PotionTile(props) {
-  return <div>PotionTile</div>;
+function PotionTile({ potion, handleClick, children }) {
+  return (
+    <div
+      className={`${
+        handleClick.name === "addToInventory" ? "card" : "inventory-card"
+      }`}
+    >
+      <div className="image-wrapper" onClick={() => handleClick(potion.id)}>
+        <img className="image" alt={potion.name} src={potion.image_url} />
+      </div>
+      {children}
+    </div>
+  );
 }
 
 export default PotionTile;
